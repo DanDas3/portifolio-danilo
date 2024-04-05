20260f2aeb5055b8cadc2598bc2af83f9f7c8b78
Danilo Araújo <daniloaraujo1990@hotmail.com>
2024-04-05 09:16:42 -0300

    Adicionado listar mensagens e limpar local storage

--- assets/js/contato.js
+++ assets/js/contato.js
@@ -1,3 +1,6 @@
+import {lerMensagensLocaStorage} from "./util.js";
+import {exibirLista} from "./mensagens.js";
+
 const formulario = document.getElementById('formulario-contato')
 
 formulario.addEventListener('submit',(e) => {
@@ -8,14 +11,24 @@ formulario.addEventListener('submit',(e) => {
   const conteudo = document.getElementById('conteudo').value
 
   const mensagem = {nome:nome,email:email,conteudo:conteudo}
-
-  const mensagensLocalStorage = localStorage.getItem('mensagens')
-  let mensagens = []
-
-  if(mensagensLocalStorage !== null) {
-    mensagens = JSON.parse(mensagensLocalStorage)
-  }
+  
+  const mensagens = lerMensagensLocaStorage()
 
   mensagens.push(mensagem)
   localStorage.setItem('mensagens',JSON.stringify(mensagens))
+  limparFormulario()
+})
+
+const limparFormulario = () => {
+  document.getElementById('nome').value = ''
+  document.getElementById('email').value = ''
+  document.getElementById('conteudo').value = ''
+}
+
+const modalMensagens = document.getElementById('modal-mensagens')
+
+modalMensagens.addEventListener('shown.bs.modal', () => {
+  const mensagens = lerMensagensLocaStorage()
+  const table = document.getElementById('lista-mensagens')
+  exibirLista(mensagens,table)
 })
